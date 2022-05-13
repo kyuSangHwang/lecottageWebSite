1da06fa0571a4996fc8af6c2c264938811631e19
dopeks <kyusang.dev@gmail.com>
2022-05-13 15:51:42 +0900

    First Commit (Create Project)

---
+++ src/main/java/com/lecottage/lecottagewebsite/LecottageWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.lecottage.lecottagewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecottageWebSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LecottageWebSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lecottage/lecottagewebsite/LecottageWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lecottage.lecottagewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecottageWebSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
