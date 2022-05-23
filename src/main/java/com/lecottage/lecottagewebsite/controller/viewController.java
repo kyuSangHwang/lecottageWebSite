5531ca749a8d5476664cdbd405e6e71b22c264d0
YooSangWoo <qswwsq11@gmail.com>
2022-05-23 11:21:28 +0900

    LeCottage  viewController    ,    main이미지

--- src/main/java/com/lecottage/lecottagewebsite/controller/viewController.java
+++ src/main/java/com/lecottage/lecottagewebsite/controller/viewController.java
@@ -12,5 +12,24 @@ public class viewController {
         return "main";
     }
 
+    @GetMapping(value = "/businessInfo")
+    public String businessInfo() { return "/lecottage/businessInfo"; }
 
+    @GetMapping(value = "/directions")
+    public String directions() { return "/lecottage/directions"; }
+
+    @GetMapping(value = "/amenity")
+    public String amenity() { return "/service/amenity"; }
+
+    @GetMapping(value = "/concierge")
+    public String concierge() { return "/service/concierge"; }
+
+    @GetMapping(value = "/contactUs")
+    public String contactUs() { return "/contactUs/contactUs"; }
+
+    @GetMapping(value = "/plan")
+    public String plan() { return "/plan/plan"; }
+
+    @GetMapping(value = "/unit")
+    public String unit() { return "/unit/unit"; }
 }
