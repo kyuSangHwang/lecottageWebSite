9adba8dae26aa865d0b2eb8a223244d842277e3c
dopeks <kyusang.dev@gmail.com>
2022-05-24 17:55:52 +0900

    header 수정 (ver 1.1)

--- src/main/resources/static/js/fragments/header.js
+++ src/main/resources/static/js/fragments/header.js
@@ -1,7 +1,69 @@
+let mobileStatusHeader = false;
+let nowPath = "";
+
+let prevScrollTop = 0;
+let nowScrollTop = 0;
 
 $(document).ready(function () {
 
+    $(window).on('scroll', function(){
+        nowScrollTop = $(this).scrollTop();
+        let upDownLocation = prevScrollTop - nowScrollTop > 0 ? 'up' : 'down';
+
+        if (!!nowScrollTop) {
+            if(upDownLocation === 'down'){
+                $('.header').addClass('on');
+            }
+            if(upDownLocation === 'up'){
+                $('.header').removeClass('on');
+            }
+        }
+        prevScrollTop = nowScrollTop;
+    });
+
+
 });
+
+function headerManager() {
+    const $header = $('.header');
+    const $headerLogo = $('.js-header-container__company-logo');
+    let pathName = location.pathname;
+
+    if (pathName != "/") {
+        $header.css('position', 'absolute');
+
+    } else {
+        $header.css('position', 'fixed');
+        $header.css('color', 'white');
+        $header.css('background-color', 'rgba(0,0,0,0)');
+        $headerLogo.css('background-image', 'url("/images/headerFooter/logoFullWidthWhite.png")');
+
+    }
+}
+
+function mainHeaderManager(index) {
+    if (mobileStatusHeader) {
+        const menu = $('#menu');
+        if (index > 0) {
+            menu.css("color", "black");
+        } else {
+            menu.css("color", "white");
+        }
+    } else {
+        const $header = $('.header');
+        const $headerLogo = $('.js-header-container__company-logo');
+        console.log(index);
+
+        if (index > 0) {
+            $header.css('color', 'black');
+            $headerLogo.css('background-image', 'url("/images/headerFooter/logoFullWidth.png")');
+        } else {
+            $header.css('color', 'white');
+            $header.css('background-color', 'rgba(0,0,0,0)');
+            $headerLogo.css('background-image', 'url("/images/headerFooter/logoFullWidthWhite.png")');
+        }
+    }
+}
 
 function mainHome() {
     locationSearch();
