d57025b965d9eb5dcea25b43cee10f68a281dfb2
YooSangWoo <qswwsq11@gmail.com>
2022-05-25 17:51:17 +0900

    amenity (ver3)

--- src/main/resources/static/js/commonJs.js
+++ src/main/resources/static/js/commonJs.js
@@ -1,24 +1,3 @@
 $(document).ready(function () {
 
 })
-
-// var swiper = new Swiper(".mySwiper", {
-//
-//     //무한루프일때, true
-//     loop: true,
-//     //페이지네이션 관련 자바스크립트
-//     pagination: {
-//         el: ".swiper-pagination",
-//         // type: "progressbar",
-//
-//     },
-//     //네이게이션 버튼 관련 자바스크립트
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-//     //
-//     autoplay: {
-//         delay: 3000,
-//     },
-// });
