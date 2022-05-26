6d9806b821352fe57e2b826a1808f16393ea1d00
dopeks <kyusang.dev@gmail.com>
2022-05-26 16:24:36 +0900

    main 개발 (ver 1: 최하단 swiper 이미지 받으면 수정해야 함)

---
+++ src/main/resources/static/js/main/main.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function () {
+
+    new Swiper(".mySwiper1", {
+        direction: 'horizontal',
+        loop: true,
+        spaceBetween: 30,
+        speed: 500,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+    new Swiper(".mySwiper2", {
+        direction: 'horizontal',
+        loop: true,
+        spaceBetween: 30,
+        speed: 500,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+
+});
+
