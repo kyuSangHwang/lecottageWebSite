7a4458729a518820bb31d852c8f6aca86be74150
dopeks <kyusang.dev@gmail.com>
2022-06-02 18:16:31 +0900

    plan 개발 중 (ver 1)

---
+++ src/main/resources/static/js/plan/plan.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function () {
+
+    const secondContentImage = $(".js-plan-container__content-wrapper__summary-box__second-content__box__img");
+    const planExplanationPart = document.querySelectorAll('.js-plan-explanation-part');
+
+    planExplanationPart.forEach( (planExplanationPart) => {
+        planExplanationPart.addEventListener('mouseover', (event) => {
+            let planExplanationPartId = event.target.getAttribute('id');
+            let partNumber = planExplanationPartId.substr(-1);
+            let backURL = '../../images/plan/planPart' + partNumber + '/' + planExplanationPartId + '.jpg';
+
+            secondContentImage.css({'background-image': 'url(' + backURL + ')'});
+            secondContentImage.css({'transition': 'all ease-in 0.5s'});
+        });
+
+        planExplanationPart.addEventListener('mouseout', (event) => {
+            secondContentImage.css({'background-image': 'url(../../images/main/main.png)'});
+            secondContentImage.css({'transition': 'all ease-in 0.5s'});
+        });
+    });
+
+});
